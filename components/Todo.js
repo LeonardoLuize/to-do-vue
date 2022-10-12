a6e5949b4a9fcb52f30b29b034a4d0d00ac865d2
LeonardoLuize <leonardo.luize2@gmail.com>
2022-10-12 14:24:00 -0300

    feat: adiciona remoção/adição de itens

--- components/Todo.js
+++ components/Todo.js
@@ -1,13 +1,19 @@
 export default {
+    props: {
+        listValue: Array,
+        addItem: Function,
+        removeItem: Function,
+        newItem: Function
+    },
     template: `
         <div class="todo-container" >
             <div class="todo-content">
                 <div class="todo-header">
-                    <input/> <button>Add</button>
+                    <input v-model="newItem" placeholder="Adicione um item" /> <button @click="addItem()">Add</button>
                 </div>
                 <div class="todo-list">
                     <ul>
-                        <li class="todo-item">item <button>X</button></li>
+                        <li class="todo-item" v-for="item in listValue" :key="item.id">{{item.text}} <button @click="removeItem(item.id)">X</button></li>
                     </ul>
                 </div>
             </div>
