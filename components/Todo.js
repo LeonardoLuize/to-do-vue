c4b581dcd8afbb741a767072648f83bb361497a5
LeonardoLuize <leonardo.luize2@gmail.com>
2022-10-12 13:12:11 -0300

    feat: adiciona estilos

---
+++ components/Todo.js
@@ -0,0 +1,16 @@
+export default {
+    template: `
+        <div class="todo-container" >
+            <div class="todo-content">
+                <div class="todo-header">
+                    <input/> <button>Add</button>
+                </div>
+                <div class="todo-list">
+                    <ul>
+                        <li class="todo-item">item <button>X</button></li>
+                    </ul>
+                </div>
+            </div>
+        </div>    
+    `
+}
