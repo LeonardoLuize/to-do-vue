ae85f61e9575e0904787b7dad6532c9f8ab76419
LeonardoLuize <leonardo.luize2@gmail.com>
2022-10-12 12:26:26 -0300

    feat: adiciona estrutura inicial

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+export default {
+    template: `
+        <div class="header-container" >
+            <div class="header-content" >
+                <h1 class="main-title"><strong class="title-bold">To.</strong>do</h1>
+            </div>
+        </div>    
+    `
+}
